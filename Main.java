7cd8e3eab52c701dc1489a638fbed6525ea7e244
Eduardo Darwich <eduardo.darwich23@gmail.com>
2026-01-02 22:11:26 -0300

    Resolvendo o desafio do leetCode LongestCommonPrefix

--- Main.java
+++ Main.java
@@ -1,15 +1,26 @@
+import Array.LongestCommonPrefix;
 import Array.TwoSum;
 
 public class Main {
     public static void main(String[] args) {
 
         //Testando alguns casos
+
+
         /*TwoSum ts = new TwoSum();
         int[] resultado = ts.twoSum(new int[]{3,2,4}, 6);
         System.out.println(resultado[0] + " " + resultado[1]);
 
         int[] resultado2 = ts.twoSum(new int[]{2,7,11,15}, 9);
         System.out.println(resultado2[0] + " " + resultado2[1]);*/
+
+
+        /*LongestCommonPrefix lcp = new LongestCommonPrefix();
+        String resultado = lcp.longestCommonPrefix(new String[]{"flor", "fluxo", "floco"});
+        System.out.println(resultado);
+
+        String resultado2 = lcp.longestCommonPrefix(new String[]{"avião", "aveia", "avelã"});
+        System.out.println(resultado2);*/
 
     }
 }
